e6571307e0905b18e8f412800bdb71e7d2c38504
anwuliagbese <jaagbese@gmail.com>
2024-09-21 11:39:23 +0100

    week 1 update

--- styles/scripts/getdates.js
+++ styles/scripts/getdates.js
@@ -1,5 +1,7 @@
-// Get the current year and display it
-document.getElementById('currentyear').textContent = new Date().getFullYear();
+// Insert the current year in the first paragraph
+const currentYear = new Date().getFullYear(); // Get the current year
+document.getElementById('currentyear').textContent = currentYear; // Set the text content of the span with id 'currentyear'
 
-// Get the last modified date of the document and display it
-document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+// Display the last modified date in the second paragraph
+const lastModifiedDate = document.lastModified; // Get the document's last modified date
+document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`; // Set the text content of the paragraph with id 'lastModified'
